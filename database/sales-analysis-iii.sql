0259d3e7addf5eb26ac959733346c6659e639653
Jingchun Wang <bettyjcwang@gmail.com>
2025-09-08 15:00:24 +0100

    LC-sales-analysis-iii [Runtime: 1393 ms; Memory: 0B]

--- database/sales-analysis-iii.sql
+++ database/sales-analysis-iii.sql
@@ -1,9 +1,13 @@
 # Write your MySQL query statement below
-select a.product_id, b.product_name 
-from Sales as a
-left join Product as b
-on a.product_id = b.product_id
-where a.product_id not in
-(select product_id
-from Sales
-where sale_date < '2019-01-01' or sale_date > '2019-03-01')
+select
+    product_id,
+    product_name
+from product
+where product_id in (
+    select
+        product_id
+    from sales
+    group by product_id
+    having max(sale_date) <= '2019-03-31'
+    and min(sale_date) >= '2019-01-01'
+)
